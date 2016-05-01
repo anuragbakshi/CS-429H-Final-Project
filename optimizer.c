289a59cecf16d0bffbf3e6bf0f784ac4a0235103
Harsh <harshgoyal@gmail.com>
2016-04-30 19:57:57 -0500

    optimizer

--- optimizer.c
+++ optimizer.c
@@ -1,4 +1,11 @@
 #include "optimizer.h"
+#include "parser.h"
+void find_semantics(Funs *funs) {
+	Fun function;
+	while(funs != 0) {
+		function = funs.first;
+	}
+}
 
 void optimize(Funs *funs) {
     find_semantics(funs);
